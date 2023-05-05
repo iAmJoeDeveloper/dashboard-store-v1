4fe9b5117d12f109f67b507fe5826acc1be2db6c
iamjoedeveloper <joesuarez.developer@gmail.com>
2023-05-05 19:25:01 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+	return (
+		<>
+			<div className='bg-indigo-500'>
+				<h1 className='text-3xl font-bold underline'>Hello World!</h1>
+			</div>
+		</>
+	)
+}
+
+export default App
