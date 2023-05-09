1142905720628a3cc6b94678f17ea88c6203d9c7
iamjoedeveloper <joesuarez.developer@gmail.com>
2023-05-09 15:29:25 -0400

    Tabs

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 	const [showOrder, setShowOrder] = useState(false)
 
 	const toggleMenu = () => {
-		setShowMenu(showMenu)
+		setShowMenu(!showMenu)
 	}
 
 	return (
@@ -39,8 +39,10 @@ function App() {
 				</nav>
 				<main className='lg:pl-28 grid grids-cols-1 lg:grid-cols-8'>
 					<div className='lg:col-span-6'>
+						{/* Header */}
 						<header className='p-4'>
-							<div className='flex flex-col gap-4'>
+							{/* Title and Search */}
+							<div className='flex flex-col gap-4 mb-6'>
 								<div>
 									<h1 className='text-2xl text-gray-300'>Jaegar Resto</h1>
 									<p className='text-gray-500'>07 October 2023</p>
@@ -51,10 +53,29 @@ function App() {
 										<input
 											type='text'
 											className='bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none'
+											placeholder='Search...'
 										/>
 									</div>
 								</form>
 							</div>
+							{/* Tabs */}
+							<nav className='text-gray-300 flex items-center justify-between border-b'>
+								<a
+									href='#'
+									className='relative py-2 pr-4 before:w-1/2 before:h-[1px] before:absolute before:bg-[#ec7c6a] before:left-0 before:rounded-full before:-bottom-[1px] text-[#ec7c6a]'
+								>
+									Hot Dishes
+								</a>
+								<a href='#' className='py-2 pr-4 '>
+									Cold Dishes
+								</a>
+								<a href='#' className='py-2 pr-4'>
+									Soup
+								</a>
+								<a href='#' className='py-2 pr-4'>
+									Grill
+								</a>
+							</nav>
 						</header>
 					</div>
 					<div className='lg:col-span-2 fixed lg:static right-0'>Carrito</div>
